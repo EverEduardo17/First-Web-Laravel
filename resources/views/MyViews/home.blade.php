1806e10a82f75f8e2c17dbb27051a1f543d28263
EverEduardo17 <evergamino17@gmail.com>
2024-08-24 15:28:31 -0600

    Modifield component alert

--- resources/views/MyViews/home.blade.php
+++ resources/views/MyViews/home.blade.php
@@ -13,10 +13,16 @@
     <div class="max-w-4xl mx-auto px-4">
         <h1>Bienvenido a la página principal</h1>
 
-        <x-MyComponents.alert>
+        <!--Se manda a llamar a nuestro componente alert -->
+        <x-MyComponents.alert type="success" class="mb-4">
+            <x-slot name="title"> <!--Colocamos el nombre de la variable definida en el componente-->
+                Alerta de seguridad <!--Esto será el valor de la variable -->
+            </x-slot>
             Contenido de la alerta
         </x-MyComponents.alert> <!--Ruta del archivo componente -->
     </div>
+
+    <p>Hola mundo</p>
 
 </body>
 </html>
